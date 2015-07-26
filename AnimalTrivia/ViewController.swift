f6bc3bfac11ccabe52a1668ffd7d5b66f0af8c38
chihoxtra <samuelpun@gmail.com>
2015-07-27 02:27:11 +0800

    logic

--- AnimalTrivia/ViewController.swift
+++ AnimalTrivia/ViewController.swift
@@ -18,11 +18,31 @@ class ViewController: UIViewController {
     
     var secondCount = 10
     let randomNumber = Int(arc4random_uniform(5))
+    
+    var correctAnswer = 0
 
 
     // IBOutlet for various UI components
     @IBOutlet weak var profilePic: UIImageView!
     @IBOutlet weak var labelTimer: UILabel!
+    @IBOutlet weak var optionButton1: UIButton!
+    @IBOutlet weak var optionButton2: UIButton!
+    @IBOutlet weak var buttonStart: UIButton!
+
+    @IBAction func buttonOption1(sender: UIButton) {
+        if correctAnswer == 0 {
+            print("對")
+            nextQuestion()
+        }
+    }
+    
+    
+    @IBAction func buttonOption2(sender: UIButton) {
+        if correctAnswer == 1 {
+            print("對")
+            nextQuestion()
+        }
+    }
     
     struct optionsAndAnswers {
         var options = [String] ()
@@ -34,14 +54,18 @@ class ViewController: UIViewController {
         var info = optionsAndAnswers()
     
     }
+    
+    var itemsList = [questions]()
 
     required init?(coder aDecoder: NSCoder) {
 
-        var itemsList:items = [
-
-        
+        itemsList = [
+            questions(picture: UIImage(named: ("0.png"))!, info: optionsAndAnswers(options: ["lo san", "小狗"], answer: 1)),
+            questions(picture: UIImage(named: ("1.png"))!, info: optionsAndAnswers(options: ["波兒", "so san"], answer: 0)),
+            questions(picture: UIImage(named: ("2.png"))!, info: optionsAndAnswers(options: ["小狗", "波兒"], answer: 1)),
+            questions(picture: UIImage(named: ("3.png"))!, info: optionsAndAnswers(options: ["so san", "lo san"], answer: 0)),
+            questions(picture: UIImage(named: ("4.png"))!, info: optionsAndAnswers(options: ["so san", "lo san"], answer: 0)),
         ]
-        
         
         
         for index in 0...(imageNumber-1) {
@@ -74,14 +98,25 @@ class ViewController: UIViewController {
         
     }
     
+    func nextQuestion() {
+        profilePic.image = itemsList[randomNumber].picture
+        optionButton1.setTitle(itemsList[randomNumber].info.options[0], forState: .Normal)
+        optionButton2.setTitle(itemsList[randomNumber].info.options[1], forState: .Normal)
+        correctAnswer = itemsList[randomNumber].info.answer
+    }
+    
     @IBAction func buttonStart(sender: AnyObject)
     {
-
-        
+        nextQuestion()
         
         print(randomNumber)
-        profilePic.image = imageList[randomNumber]
+
         
+        buttonStart.hidden = true
+        optionButton1.hidden = false
+        optionButton2.hidden = false
+        
+    
     }
 
 }
