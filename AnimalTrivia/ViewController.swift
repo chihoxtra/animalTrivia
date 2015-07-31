b8d79ceae45865a7b67831149a4aaa305145a01b
chihoxtra <samuelpun@gmail.com>
2015-07-31 20:27:13 +0800

    additional logic on wrong answer

--- AnimalTrivia/ViewController.swift
+++ AnimalTrivia/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
     var secondCount = 10
     var correctAnswer = 0
     var dataURL = "http://chihoxtra.ddns.net/images/games/items.txt"
+    var score = 0
 
 
     // IBOutlet for various UI components
@@ -26,19 +27,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var optionButton1: UIButton!
     @IBOutlet weak var optionButton2: UIButton!
     @IBOutlet weak var buttonStart: UIButton!
+    @IBOutlet weak var labelScore: UILabel!
 
+    @IBOutlet weak var labelCorrect: UILabel!
+    
     @IBAction func buttonOption1(sender: UIButton) {
         if correctAnswer == 0 {
             print("對")
+            labelCorrect.hidden = true
+            score++
+            labelScore.text = "Score:" + String(score)
             nextQuestion()
+        } else {
+            labelCorrect.hidden = false
         }
     }
     
     
     @IBAction func buttonOption2(sender: UIButton) {
         if correctAnswer == 1 {
+            labelCorrect.hidden = true
             print("對")
+            score++
+            labelScore.text = "Score:" + String(score)
             nextQuestion()
+        } else {
+            labelCorrect.hidden = false
         }
     }
     
@@ -58,7 +72,7 @@ class ViewController: UIViewController {
     func prepareData() {
         
         
-        let dataSource = NSURL(string: dataURL)
+//        let dataSource = NSURL(string: dataURL)
 
 //        let task = NSURLSession.sharedSession().dataTaskWithURL(dataSource!) {
 //            (data, response, error) in print(NSString(data: data!, encoding: NSUTF8StringEncoding))
@@ -68,41 +82,41 @@ class ViewController: UIViewController {
         
         //        let rawData = NSData(contentsOfURL:dataSource!)
         
-        var bufferString:NSString = ""
+//        var bufferString:NSString = ""
+//        
+//        while (bufferString.length == 0) {
+//            do {
+//                bufferString = try NSString(contentsOfURL: dataSource!, encoding: NSUTF8StringEncoding)
+//
+//            } catch {
+//                // Handle Error
+//                print("error in fetching the data")
+//            }
+//        }
+//        var dataLine = [String](), dataItems = [String]()
+//        
+//        dataLine = bufferString.componentsSeparatedByString("\n1")
+//        
+//        for i in 0...dataLine.count {
+//            dataItems = dataLine[i].componentsSeparatedByString(",")
+//            itemsList.append(questions(picture: UIImage(named: (dataItems[0]))!, info: optionsAndAnswers(options: [dataItems[1], dataItems[2]], answer: Int(dataItems[3])!)))
+//            print(itemsList)
+//        }
         
-        while (bufferString.length == 0) {
-            do {
-                bufferString = try NSString(contentsOfURL: dataSource!, encoding: NSUTF8StringEncoding)
-
-            } catch {
-                // Handle Error
-                print("error in fetching the data")
-            }
-        }
-        var dataLine = [String](), dataItems = [String]()
         
-        dataLine = bufferString.componentsSeparatedByString("\n1")
-        
-        for i in 0...dataLine.count {
-            dataItems = dataLine[i].componentsSeparatedByString(",")
-            itemsList.append(questions(picture: UIImage(named: (dataItems[0]))!, info: optionsAndAnswers(options: [dataItems[1], dataItems[2]], answer: Int(dataItems[3])!)))
-            print(itemsList)
-        }
-        
-        
-//        itemsList = [
-//            questions(picture: UIImage(named: ("0.png"))!, info: optionsAndAnswers(options: ["lo san", "小狗"], answer: 1)),
-//            questions(picture: UIImage(named: ("1.png"))!, info: optionsAndAnswers(options: ["波兒", "so san"], answer: 0)),
-//            questions(picture: UIImage(named: ("2.png"))!, info: optionsAndAnswers(options: ["小狗", "波兒"], answer: 1)),
-//            questions(picture: UIImage(named: ("3.png"))!, info: optionsAndAnswers(options: ["so san", "lo san"], answer: 1)),
-//            questions(picture: UIImage(named: ("4.png"))!, info: optionsAndAnswers(options: ["so san", "lo san"], answer: 0)),
-//        ]
+        itemsList = [
+            questions(picture: UIImage(named: ("0.png"))!, info: optionsAndAnswers(options: ["lo san", "小狗"], answer: 1)),
+            questions(picture: UIImage(named: ("1.png"))!, info: optionsAndAnswers(options: ["波兒", "so san"], answer: 0)),
+            questions(picture: UIImage(named: ("2.png"))!, info: optionsAndAnswers(options: ["小狗", "波兒"], answer: 1)),
+            questions(picture: UIImage(named: ("3.png"))!, info: optionsAndAnswers(options: ["so san", "lo san"], answer: 1)),
+            questions(picture: UIImage(named: ("4.png"))!, info: optionsAndAnswers(options: ["so san", "lo san"], answer: 0)),
+        ]
     }
     
     
     required init?(coder aDecoder: NSCoder) {
-
         super.init(coder: aDecoder)
+        score = 0
         self.prepareData()
     }
     
@@ -124,14 +138,17 @@ class ViewController: UIViewController {
         
         if(secondCount > 0)
         {
+            if(secondCount < 3) {
+                labelTimer.textColor = UIColor.redColor()
+            }
             labelTimer.text = "Time Left" + String(--secondCount)
         } else {
+            labelTimer.text = "Time is up了"
             buttonStart.setTitle("再來噢", forState: .Normal)
             buttonStart.hidden = false
             optionButton1.hidden = true
             optionButton2.hidden = true
             profilePic.hidden = true
-            labelTimer.hidden = true
             prepareData()
 
         }
@@ -159,6 +176,7 @@ class ViewController: UIViewController {
         buttonStart.hidden = true
         optionButton1.hidden = false
         optionButton2.hidden = false
+        labelScore.hidden = false
         
     
     }
